a3f55fe5e09a4db495301e4907bd1eb5d3da1ae9
yarmand <yann@harakys.com>
2026-07-13 09:04:22 -0700

    feat(remote): add remote serve command entrypoint
    Wire `tsession remote serve` through main.go dispatch and cmd.Remote,
    running the RPC daemon over stdin/stdout.

---
+++ cmd/remote.go
@@ -0,0 +1,26 @@
+package cmd
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/yarma/tsession/internal/remote"
+)
+
+// Remote dispatches `tsession remote <subcommand>`.
+func Remote(args []string) error {
+	fs := flag.NewFlagSet("remote", flag.ContinueOnError)
+	if err := fs.Parse(args); err != nil {
+		return err
+	}
+	if fs.NArg() < 1 {
+		return fmt.Errorf("usage: tsession remote <serve>")
+	}
+	switch fs.Arg(0) {
+	case "serve":
+		return remote.Serve(os.Stdin, os.Stdout)
+	default:
+		return fmt.Errorf("unknown remote subcommand: %s", fs.Arg(0))
+	}
+}
